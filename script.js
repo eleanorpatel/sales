61253d91ff97c56618fa25eff5da2894c632b081
eleanorpatel <eleanor.v.patel@gmail.com>
2025-12-01 18:36:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const add-buttons = document.querySelectorAll(`.add-btn`);
 
+const cart-list = document.getElementById(`cart-items`);
+
+const cart-total = document.getElementById(`.cart-total`);
+
+const clear-cart = document.getElementById(`.clear-cart`);
+
+let cart = []
+  addButtons.forEach(btn => { btn.addEventListener ("click", () => {const name = btn.dataset.name;
+                                                                    const price = parseFloat (btn.dataset.price);
+                                                                    const existing-item = cart.find (i => i.name === name);
+                                                                    if (existing-item) {existing-item.quantity += 1;
+                                                                                       } else {
+                                                                                        cart.push ({name, price, quantity: 1}):
+                                                                    }
+                                                                    updateCart();
+                                                                   })})
+function updateCart (){
+  cartList.innerHTML = "";
+  let total = 0;
+  cart.forEach (item, index => { const li = document.createElement("li");
+                                li.classList.add (cart-item)
+                                li.innerHTML = ${item.name} x${item.quantity} - $${(item.price * item.quantity).toFixed(2)}
+                                <button onclick="removeItem(${index})">Remove</button>
+                ;
+  cartList.appendChild(li);
+  total += item.price * item.quantity;
+});
+cartTotal.textContent = Total: $${total.toFixed(2)}
+}
